626001e3a92f16c389497c34b414f08aa711ae2b
SarrthiDellXps15 <sarrthijasrotia05@gmail.com>
2022-08-13 20:37:36 -0500

    working on the switch turn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 /*----- constants -----*/
+let player1 = "O";
+let player2 = "X";
+let currentTurn = player1;
+
+
+whoseTurn.innerText = ""
+
 /*----- app's state (variables) -----*/
+
+
+
 /*----- cached element references -----*/
+
+let whoseTurn = document.getElementById("turnText");
+let cells = document.getElementsByClassName("cell")
+
 /*----- event listeners -----*/
+
+
+
 /*----- functions -----*/
+cells.forEach(
+    function(){
+        .addEventListener
+    }
+)
