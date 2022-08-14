a0967696181a5fc9b9ddb801c634e3172d2e46ec
SarrthiDellXps15 <sarrthijasrotia05@gmail.com>
2022-08-14 14:48:24 -0500

    code format adjustments

--- js/script.js
+++ js/script.js
@@ -15,14 +15,14 @@ let cells = document.querySelectorAll(".cell");
 cellArray = Array.from(cells)
 
 
- 
+
 
 /*----- functions -----*/
 cells.forEach(function (cell) {
     cell.addEventListener('click', function () {
         cell.innerHTML = (currentPlayer)
         currentPlayer = currentPlayer = currentPlayer == player1 ? player2 : player1;
-        document.getElementById("turnTxt").innerText ="It is "+currentPlayer + "'s turn"
+        document.getElementById("turnTxt").innerText = "It is " + currentPlayer + "'s turn"
 
     })
 })
